863b6eeb9979871cb43dcc3283a3557ff294b4eb
hubenlv <benlv.hu@qq.com>
2021-01-08 17:30:33 +0800

    Connect Wallet

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,11 @@
 import { Component } from "react";
-import { Menu } from "antd";
+import { Menu, Icon, Row, Col } from "antd";
 import styles from "./style.module.less";
 
 const { SubMenu } = Menu;
 
 export default class Header extends Component {
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   state = {
     current: "mail",
@@ -22,28 +21,42 @@ export default class Header extends Component {
   render() {
     return (
       <div className={styles.root}>
-        <Menu
-          onClick={this.handleClick}
-          selectedKeys={[this.state.current]}
-          mode="horizontal"
-        >
-          <Menu.Item key="logo" className={styles.dderivatives}>DDerivatives</Menu.Item>
-          <Menu.Item key="trade">Trade</Menu.Item>
-          <Menu.Item key="pool">Pool</Menu.Item>
-          <SubMenu title={<span className="submenu-title-wrapper">DDS</span>}>
-            <Menu.ItemGroup title="Item 1">
-              <Menu.Item key="setting:1">Option 1</Menu.Item>
-              <Menu.Item key="setting:2">Option 2</Menu.Item>
-            </Menu.ItemGroup>
-            <Menu.ItemGroup title="Item 2">
-              <Menu.Item key="setting:3">Option 3</Menu.Item>
-              <Menu.Item key="setting:4">Option 4</Menu.Item>
-            </Menu.ItemGroup>
-          </SubMenu>
-          <Menu.Item key="broker">Broker</Menu.Item>
-          <Menu.Item key="analytics">Analytics</Menu.Item>
-          <Menu.Item key="support">Support</Menu.Item>
-          {/* <Menu.Item key="alipay">
+        <Row  type="flex" justify="space-between" align="middle">
+          <Col span={20}>
+            <Menu
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+            >
+              <Menu.Item key="logo" className={styles.dderivatives}>
+                DDerivatives
+              </Menu.Item>
+              <Menu.Item key="trade">Trade</Menu.Item>
+              <Menu.Item key="pool">Pool</Menu.Item>
+              <SubMenu
+                title={
+                  <span className="submenu-title-wrapper">
+                    DDS&nbsp;&nbsp;
+                    <Icon type="down" />
+                  </span>
+                }
+              >
+                <Menu.ItemGroup title="Item 1">
+                  <Menu.Item key="setting:1">Option 1</Menu.Item>
+                  <Menu.Item key="setting:2">Option 2</Menu.Item>
+                </Menu.ItemGroup>
+                <Menu.ItemGroup title="Item 2">
+                  <Menu.Item key="setting:3">Option 3</Menu.Item>
+                  <Menu.Item key="setting:4">Option 4</Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+              <Menu.Item key="broker">Broker</Menu.Item>
+              <Menu.Item key="analytics">Analytics</Menu.Item>
+              <Menu.Item key="support">
+                Support&nbsp;&nbsp;
+                <Icon type="down" />
+              </Menu.Item>
+              {/* <Menu.Item key="alipay">
             <a
               href="https://ant.design"
               target="_blank"
@@ -52,7 +65,12 @@ export default class Header extends Component {
               Navigation Four - Link
             </a>
           </Menu.Item> */}
-        </Menu>
+            </Menu>
+          </Col>
+          <Col span={4} className={styles.connectWpr}>
+            <div className={styles.connectBtn}><span>Connect Wallet</span></div>
+          </Col>
+        </Row>
       </div>
     );
   }
