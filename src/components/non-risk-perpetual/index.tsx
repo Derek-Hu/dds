f46be9282f7e91b41b861a377052cf6382946b9d
hubenlv <benlv.hu@qq.com>
2021-01-08 10:51:20 +0800

    trading

--- src/components/non-risk-perpetual/index.tsx
+++ src/components/non-risk-perpetual/index.tsx
@@ -6,16 +6,24 @@ const columns = [
     title: "Coin",
     dataIndex: "coin",
     key: "coin",
+    render: (coin: string) => {
+        return <span>
+            <span className={styles.coin}>{coin}</span><span className={styles.usdt}> / USDT</span>
+        </span>
+    }
   },
   {
-    title: "Last Price",
+    title: <span  className={styles.price}>Last Price</span>,
     dataIndex: "price",
     key: "price",
+    render: (price: number) => <span className={styles.priceVal}>{price}</span>
   },
   {
-    title: "24h Change",
+    title: <span  className={styles.change}>24h Change</span>,
     dataIndex: "change",
     key: "change",
+    width: '10em',
+    render: (percentage: any) => <span className={[styles.changeVal, percentage<0 ? styles.negative: ''].join(' ')}>{percentage>0?'+':''}{percentage}%</span>
   },
   {
     title: "Chart",
@@ -26,6 +34,7 @@ const columns = [
     title: "Action",
     dataIndex: "action",
     key: "action",
+    width: '6em'
   },
 ];
 
@@ -33,7 +42,12 @@ const data = [
   {
     coin: "BTC",
     price: "7173.77",
-    change: "8.23",
+    change: 8.23,
+  },
+  {
+    coin: "ETH",
+    price: "183.28",
+    change: -10.01,
   },
 ];
 
