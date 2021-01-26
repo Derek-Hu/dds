9a1924f0c2e56206552a3e3c48483788d1468774
hubenlv <benlv.hu@qq.com>
2021-01-26 21:17:31 +0800

    Close Order

--- src/components/trade-bonus/modals/order-close.tsx
+++ src/components/trade-bonus/modals/order-close.tsx
@@ -1,32 +1,38 @@
 import { Form, Button, Row, Col, Select, Input } from "antd";
-import { CustomTabKey, SupportedCoins } from "../../../constant/index";
-import styles from "./style.module.less";
+import styles from "../../funding-balance/modals/style.module.less";
 import ModalRender from "../../modal-render/index";
 
-const { Option } = Select;
-
-const title = "Funding Withdraw";
+const title = "Close Order";
 
 export default (props: any) => {
   const { visible, onCancel } = props;
   return (
-    <ModalRender visible={visible} onCancel={onCancel} footer={null}>
+    <ModalRender
+      visible={visible}
+      onCancel={onCancel}
+      footer={null}
+      className={styles.commonModal}
+    >
       <h4>{title}</h4>
-      <Row>
-        <Form.Item>
+      <Row type="flex" justify="space-between" align="middle">
+        <Col xs={20} sm={20} md={20} lg={20}>
           <Input
             className={styles.ddsInput}
             placeholder="How many DDS do you want to swap and burn?"
           />
+        </Col>
+        <Col xs={4} sm={4} md={4} lg={4} style={{    textAlign: 'center'}}>
           <span className={styles.unit}>DDS</span>
-        </Form.Item>
+        </Col>
       </Row>
-      <p>Total Amount: 10.36 <Button type="link">Close All</Button></p>
-      <Row>
-        <Col>
+      <p>
+        Total Amount: 10.36 <Button type="link">Close All</Button>
+      </p>
+      <Row className={styles.actionBtns} gutter={16}>
+        <Col xs={24} sm={24} md={12} lg={12}>
           <Button>Cancel</Button>
         </Col>
-        <Col>
+        <Col xs={24} sm={24} md={12} lg={12}>
           <Button type="primary">Close</Button>
         </Col>
       </Row>
