012b30aceb1e8f6a1fb4b0e5d5147a428ccf3f5c
hubenlv <benlv.hu@qq.com>
2021-02-01 00:46:26 +0800

    deploy

--- src/routers/render.tsx
+++ src/routers/render.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 export interface IRouter {
   path: string;
