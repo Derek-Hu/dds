88285d38d6f25952b23924d593f0f4838205db6f
hubenlv <benlv.hu@qq.com>
2021-03-14 17:05:38 +0800

    tooltip

---
+++ src/components/mask/index.tsx
@@ -0,0 +1,47 @@
+import styles from './style.module.less';
+import { Icon, Button, } from 'antd';
+import success from '../../assets/images/success.png';
+import pending from '../../assets/images/pending.png';
+import fail from '../../assets/images/fail.png';
+import ReactDOM from 'react-dom';
+
+export default {
+  dom: null,
+
+  $run(url: string){
+      // @ts-ignore
+      this.close();
+      // @ts-ignore
+      this.dom = document.createElement('div');
+  
+      const JSXdom = <div className={styles.tooltip}>
+      <div>
+        <span onClick={this.hide} className={styles.close}>
+          <Icon type="close" />
+        </span>
+        <div className={styles.imgContent}>
+          <img src={url} alt=''/>
+        </div>
+        <p>{url === fail ? 'Failed' : url === success ?  'Success' : ''}</p>
+        { url === pending ? null : <Button>Done</Button>}
+      </div>
+    </div>;
+      // @ts-ignore
+      ReactDOM.render(JSXdom, this.dom);
+      // @ts-ignore
+      document.body.appendChild(this.dom);
+  },
+  showLoading() {
+    this.$run(pending)
+  },
+  showSuccess() {
+    this.$run(success)
+  },
+  showFail() {
+    this.$run(fail)
+  },
+  hide() {
+      // @ts-ignore
+    this.dom && this.dom.remove();
+  },
+};
