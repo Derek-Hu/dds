acb47a6a5b65cdf32bb47d4dfcb09708fee731b1
hubenlv <benlv.hu@qq.com>
2021-04-01 08:52:30 +0800

    文案 & 打包

--- src/components/sections/community-drived/says.ts
+++ src/components/sections/community-drived/says.ts
@@ -2,11 +2,11 @@ export default [
   {
     name: 'Roger Spears',
     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description: 'We are waiting for your testimonial about shield~',
+    description: 'We are waiting for your testimonials about shield~',
   },
-  {
-    name: 'Roger Spears2',
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description: 'Welcome anyone who wants to promote the evolution',
-  },
+  // {
+  //   name: 'Roger Spears2',
+  //   avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+  //   description: 'Welcome anyone who wants to promote the evolution',
+  // },
 ];
