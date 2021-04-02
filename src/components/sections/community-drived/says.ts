90cdb50f2bedbe79d9f99a51dea044b73c84509c
hubenlv <benlv.hu@qq.com>
2021-04-02 09:11:33 +0800

    sasys

--- src/components/sections/community-drived/says.ts
+++ src/components/sections/community-drived/says.ts
@@ -1,12 +1,12 @@
 export default [
   {
-    name: 'Roger Spears',
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description: 'We are waiting for your testimonials about shield~',
+    name: 'Ruby',
+    avatar: '/ruby.png',
+    description: 'Opened my long BTC position and deposit my dai for gain interest, smooth and simple',
   },
   // {
-  //   name: 'Roger Spears2',
-  //   avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-  //   description: 'Welcome anyone who wants to promote the evolution',
+  // name: 'Roger Spears2',
+  // avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+  // description: 'We are waiting for your testimonials about shield~',
   // },
 ];
