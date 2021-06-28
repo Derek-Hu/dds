d9ddaf0a862597c05eac47a635bfa08dc5125b84
garlic <garlic.william@gmail.com>
2021-06-28 16:31:58 +0800

    temp save

--- src/components/activities/claim-test/claim-test-token.tsx
+++ src/components/activities/claim-test/claim-test-token.tsx
@@ -7,6 +7,7 @@ import { from } from 'rxjs';
 import { finalize, switchMap } from 'rxjs/operators';
 import { Col, message, Row } from 'antd';
 import styles from './claim-test-token.module.less';
+import SiteContext, { ISiteContextProps } from '../../../layouts/SiteContext';
 
 type IProps = {
   visibleChange?: (visible: boolean) => void;
@@ -19,6 +20,8 @@ type IState = {
 };
 
 export class ClaimTestToken extends Component<IProps, IState> {
+  static contextType = SiteContext;
+
   state: IState = {
     isPopUp: false,
     isClaimed: false,
@@ -76,46 +79,50 @@ export class ClaimTestToken extends Component<IProps, IState> {
 
   render() {
     return (
-      <>
-        <div onClick={() => this.changePopUp(true)}>{this.props.children}</div>
-        <ModalRender
-          visible={this.state.isPopUp}
-          title="Claim Test ShieldDAI"
-          footer={null}
-          onCancel={() => this.changePopUp(false)}
-        >
-          <div>
-            <Row>
-              <Col xs={24} sm={24} md={24} lg={24}>
-                <span className={styles.address}>Amount:</span>
-                <span className={styles.addressHex}>5000</span>
-                <br />
-                <span className={styles.address}>Address:</span>
-                <span className={styles.addressHex}>{this.state.curAddress}</span>
-              </Col>
-            </Row>
+      <SiteContext.Consumer>
+        {(context: ISiteContextProps) => (
+          <>
+            <div onClick={() => this.changePopUp(true)}>{this.props.children}</div>
+            <ModalRender
+              visible={this.state.isPopUp}
+              title="Claim Test ShieldDAI"
+              footer={null}
+              onCancel={() => this.changePopUp(false)}
+            >
+              <div>
+                <Row>
+                  <Col xs={24} sm={24} md={24} lg={24}>
+                    <span className={styles.address}>Amount:</span>
+                    <span className={styles.addressHex}>5000</span>
+                    <br />
+                    <span className={styles.address}>Address:</span>
+                    <span className={styles.addressHex}>{this.state.curAddress}</span>
+                  </Col>
+                </Row>
 
-            <Row gutter={[8, 8]}>
-              <Col xs={24} sm={24} md={12} lg={12}>
-                <NormalButton type={'default'} onClick={() => this.changePopUp(false)}>
-                  CANCEL
-                </NormalButton>
-              </Col>
+                <Row gutter={[8, 8]}>
+                  <Col xs={24} sm={24} md={12} lg={12}>
+                    <NormalButton type={'default'} onClick={() => this.changePopUp(false)}>
+                      CANCEL
+                    </NormalButton>
+                  </Col>
 
-              <Col xs={24} sm={24} md={12} lg={12}>
-                <NormalButton
-                  type={'primary'}
-                  onClick={this.onClaim.bind(this)}
-                  loading={this.state.isPending}
-                  disabled={this.state.isClaimed}
-                >
-                  {this.state.isClaimed ? <span>CLAIMED</span> : <span>CLAIM</span>}
-                </NormalButton>
-              </Col>
-            </Row>
-          </div>
-        </ModalRender>
-      </>
+                  <Col xs={24} sm={24} md={12} lg={12}>
+                    <NormalButton
+                      type={'primary'}
+                      onClick={this.onClaim.bind(this)}
+                      loading={this.state.isPending}
+                      disabled={this.state.isClaimed}
+                    >
+                      {this.state.isClaimed ? <span>CLAIMED</span> : <span>CLAIM</span>}
+                    </NormalButton>
+                  </Col>
+                </Row>
+              </div>
+            </ModalRender>
+          </>
+        )}
+      </SiteContext.Consumer>
     );
   }
 }
