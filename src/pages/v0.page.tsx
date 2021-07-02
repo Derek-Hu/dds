fba38ac7ffa31eba6be5772654365acce468e90c
hubenlv <benlv.hu@qq.com>
2021-07-02 21:17:13 +0800

    landing backed by

--- src/pages/v0.page.tsx
+++ src/pages/v0.page.tsx
@@ -1,24 +1,24 @@
-// import { Component } from 'react';
-// import CommunityDrived from '../components/sections/community-drived/index';
-// import WhyDderivatives from '../components/sections/why-dderivatives/index';
-// import ContactUs from '../components/sections/contact-us/index';
-// import WhatIs from '../components/sections/what-is-dderivatives/index';
-// import NonRisk from '../components/non-risk-perpetual/index';
-// // import HowTrade from '../components/sections/how-to-trade/index';
-// import Banner from '../components/banner/index';
+import { Component } from 'react';
+import CommunityDrived from '../components/sections/community-drived/index';
+import WhyDderivatives from '../components/sections/why-dderivatives/index';
+import ContactUs from '../components/sections/contact-us/index';
+import WhatIs from '../components/sections/what-is-dderivatives/index';
+import NonRisk from '../components/non-risk-perpetual/index';
+// import HowTrade from '../components/sections/how-to-trade/index';
+import Banner from '../components/banner/index';
 
-// export default class HomePage extends Component {
-//   render() {
-//     return (
-//       <div style={{ background: '#fff' }}>
-//         <Banner></Banner>
-//         <WhatIs></WhatIs>
-//         {/* <NonRisk></NonRisk> */}
-//         {/* <HowTrade></HowTrade> */}
-//         <WhyDderivatives />
-//         <CommunityDrived />
-//         <ContactUs />
-//       </div>
-//     );
-//   }
-// }
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div style={{ background: '#fff' }}>
+        <Banner></Banner>
+        <WhatIs></WhatIs>
+        {/* <NonRisk></NonRisk> */}
+        {/* <HowTrade></HowTrade> */}
+        <WhyDderivatives />
+        <CommunityDrived />
+        <ContactUs />
+      </div>
+    );
+  }
+}
