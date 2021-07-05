9509ff06ecadef7d0ff95e5f6f77909709a84ee9
hubenlv <benlv.hu@qq.com>
2021-07-05 23:50:19 +0800

    typing.js

--- src/components/sections/shield-dao/index.tsx
+++ src/components/sections/shield-dao/index.tsx
@@ -1,4 +1,5 @@
 import { Row, Col } from 'antd';
+import { useEffect, useState } from 'react';
 import styles from './style.module.less';
 import SiteContext from '../../../layouts/SiteContext';
 import AdsImage from '../../../assets/shield-dao.png';
@@ -15,14 +16,51 @@ const adsImage = (
 );
 
 export default () => {
+  // const [message, setMesg] = useState<string>('Company');
+
+  // useEffect(() => {
+  //   const doAnimation = (source: any, output: any) => {
+  //     return new Promise(reslove => {
+  //       // eslint-disable-next-line
+  //       const typing = new Typing({
+  //         source,
+  //         output,
+  //         delay: 80,
+  //         done: function () {
+  //           reslove(null);
+  //         }, //完成打印后的回调事件
+  //       });
+  //       typing.start();
+  //     });
+  //   };
+  //   const doAll = async () => {
+  //     // eslint-disable-next-line
+  //     while(true){
+  //       await doAnimation('start', 'output');
+  //       await doAnimation('start', 'output');
+  //       await doAnimation('start', 'output');
+  //     }
+  //   }
+
+  //   doAll();
+
+  // }, []);
+
   return (
     <SiteContext.Consumer>
       {({ isMobile }) => (
         <div className={styles.shieldDao}>
           {/* <img alt="" src={AdsImage} width="56%" /> */}
+          <p>Company/Foundation/Business organization</p>
           <div className={styles.bgImg}>
             <div>
-              <h3>Shield DAO</h3>
+              <h3>
+                Shield DAO is not a
+                <p id="output-wrap">
+                  <span id="output"></span>
+                  <span className="typing-cursor">|</span>
+                </p>
+              </h3>
               <p>ShieldDAO is committed to becoming an open global decentralized governance organization.</p>
               <div className={carouselStyles.carousel}>
                 <Carousel autoplay>
