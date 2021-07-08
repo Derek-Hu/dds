94c21028d3a3dbafc3528fdaca79e455dcca85aa
hubenlv <benlv.hu@qq.com>
2021-07-08 21:58:19 +0800

    rem

--- src/entry/landing.js
+++ src/entry/landing.js
@@ -3,4 +3,7 @@ import '../landing.less';
 import landingSettings from '../routers/landing';
 import render from '../routers/render';
 
+const currentW = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+window.document.documentElement.style.fontSize = parseInt((currentW / 1920) * 100) + 'px';
+
 ReactDOM.render(render(landingSettings, '/home'), document.getElementById('root'));
